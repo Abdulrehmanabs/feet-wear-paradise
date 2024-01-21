f28c06b43b98bd37522a1ddf7dc22039ff4be40f
Abdulrehmanabs <abdulrehmanabs786@gmail.com>
2024-01-21 14:37:41 +0500

    Fully tested API's

--- app.js
+++ app.js
@@ -4,15 +4,10 @@ let app = Express();
 const cors = require("cors");
 let jwt = require('jsonwebtoken')
 require("dotenv").config(); //library call for using .env in node.js
+
 const path = require('path')
 
 
-
-
-// Use body-parser middleware for Express.Routes to parse JSON and form data
-// const bodyParser = require('body-parser');
-// app.use(bodyParser.json());
-// app.use(bodyParser.urlencoded({ extended: true }));
 
 
 app.use(cors()); //cors for frontend compatibility
@@ -23,13 +18,11 @@ app.use(Express.urlencoded({ extended: true }))  // parse form data
 
 // middleware for build folder in Server
 // and also required for publishing site
-app.use(Express.static(path.join(__dirname, 'build')))
-
 app.use(Express.static('./build'));
+// app.use(Express.static(path.join(__dirname, 'build')))
 
-// app.get('*', (req, res) => {
-//     res.sendFile(path.join(__dirname, 'build', 'index.html'))
-// })
+
+
 
 
 
@@ -48,26 +41,29 @@ app.post("/create-user", async (req, res) => {
 // log in with token
 app.post("/login-user", async (req, res) => {
     let logUser = users.find((user) => user.email === req.body.email && user.password === req.body.password)
-    // console.log(logUser);
+    console.log(logUser);
     if (logUser) {
         // generate token by jsonjwt library
         jwt.sign({ ...logUser }, 'Authenticated by ABS', { expiresIn: '1d' }, function (err, token) {
             res.json({ logUser, token });
         })
     } else {
-        res.json('User Not Found'); // important for toast of error if user not found
+        res.json({ success: 'User Not Found' }); // important for toast of error if user not found
     }
 })
 
 // verify token
 app.get("/user-token", async (req, res) => {
-    // console.log(req.query);
-    // Verify token by jsonjwt library
-    jwt.verify(req.query.token, 'Authenticated by ABS', async function (error, tokenData) {
-        // console.log(tokenData);
-        let verifyedUser = users.find((user) => user.email === tokenData.email && user.password === tokenData.password)
-        res.json(verifyedUser);
-    })
+    console.log(req.query);
+
+    if (req.query) {
+        // Verify token by jsonjwt library
+        jwt.verify(req.query.token, 'Authenticated by ABS', async function (error, tokenData) {
+            // console.log(tokenData);
+            let verifyedUser = users.find((user) => user.email === tokenData.email && user.password === tokenData.password)
+            res.json({ success: verifyedUser });
+        })
+    }
 })
 
 
@@ -75,7 +71,7 @@ app.get("/user-token", async (req, res) => {
 app.post("/ordered_products", async (req, res) => {
 
     const { Orders } = req.body;
-    // console.log(Orders);
+    console.log(Orders);
     orders = [...Orders, ...orders]
     res.json({ success: true })
 })
@@ -84,14 +80,26 @@ app.post("/ordered_products", async (req, res) => {
 
 
 // pending page route
-app.get("/pending_orders", async (req, res) => {
-    res.json({ success: orders });
+app.delete("/get-pending-orders", async (req, res) => {
+    console.log(req.query);
+    if (req.query) {
+        res.json({ success: orders });
+    }
 })
 
-// error handler
+
+
+// error handler recomended in testing & server
+// always use in bottom because there are more routes of get 
+// that can be distroyed by this so use path '/' in below
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'build', 'index.html'))
+})
+
 // app.get('*', (req, res) => {
 //     res.send('404 not found from server')
 // })
+
 
 
 
